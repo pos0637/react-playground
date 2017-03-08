21c7fed63229e7cc5f6bc9206635ba6c008727e2
pos0637 <pos0637@163.com>
2017-03-08 08:50:51 +0800

    使用amazeui作为组件库

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 
 import React, { Component } from 'react';
-import { DatePicker } from 'antd';
+import { DateTimeInput } from 'amazeui-react';
 import logo from './logo.svg';
 import './App.css';
-import 'antd/dist/antd.css';
 
-class App extends Component {
+export default class App extends Component {
     render() {
         return (
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h2>Welcome to React</h2>
-                    <DatePicker />
+                    <div style={{width: 200}}>
+                        <DateTimeInput dateTime="2015-05-20 12:12" />
+                    </div>
                 </div>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+                </p>
             </div>
         );
     }
 }
-
-export default App;
